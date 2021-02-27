10cea6374b2270c82cd1561332628503f2ca3a22
unknown <jannathsyeda7@gmail.com>
2021-02-28 02:22:42 +0600

    fetch and display countries and capital

--- app.js
+++ app.js
@@ -5,17 +5,30 @@ fetch('https://restcountries.eu/rest/v2/all')
 const displayCountries= countries=>{
 
     const contriesDiv=document.getElementById('countries');
-    for(let i=0; i< countries.length; i++){
-        const country=countries[i];
+    // for(let i=0; i< countries.length; i++){
+    //     const country=countries[i];
+    //     const countryDiv=document.createElement('div');
+    //     const h3= document.createElement('h3');
+    //     h3.innerText =country.name;
+    //     const p=document.createElement('p');
+    //     p.innerText=country.capital;
+    //     countryDiv.appendChild(h3);
+    //     countryDiv.appendChild(p);
+    //     contriesDiv.appendChild(countryDiv);
+
+
+    // }
+    countries.forEach(country => {
         const countryDiv=document.createElement('div');
-        const h3= document.createElement('h3');
-        h3.innerText =country.name;
-        const p=document.createElement('p');
-        p.innerText=country.capital;
-        countryDiv.appendChild(h3);
-        countryDiv.appendChild(p);
+        countryDiv.className="country";
+        const countryInfo=`
+        <h3 class="country-name">${country.name}</h3>
+        <p>${country.capital}</p>
+        `;
+        countryDiv.innerHTML=countryInfo;
         contriesDiv.appendChild(countryDiv);
 
+    });
 
-    }
+    
 }
