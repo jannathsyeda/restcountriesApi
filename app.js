43d3cc7cd7a90cfb0a2f26e53e7cfe8b5258b3ba
unknown <jannathsyeda7@gmail.com>
2021-03-02 04:04:11 +0600

    display image,details onclick

--- app.js
+++ app.js
@@ -24,11 +24,38 @@ const displayCountries= countries=>{
         const countryInfo=`
         <h3 class="country-name">${country.name}</h3>
         <p>${country.capital}</p>
+        <button onclick="displayCountryDetail('${country.name}')">details</button>
         `;
         countryDiv.innerHTML=countryInfo;
         contriesDiv.appendChild(countryDiv);
 
     });
 
-    
-}
+}  
+const  displayCountryDetail= name =>{
+        const url=`https://restcountries.eu/rest/v2/name/${name}`;
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>renderCountryInfo(data[0]));
+        
+
+    }
+
+    const renderCountryInfo= country =>{
+        const countryDiv=document.getElementById('countryDetail');
+        countryDiv.innerHTML= ` 
+        <h1>${country.name}</h1>
+        <p>population: ${country.population}</p>
+        <p>Area: ${country.area}</p>
+        <img src="${country.flag}">
+        `
+    }
+    // const renderCountryInfo = country => {
+    //     const countryDiv = document.getElementById('countryDetail');
+    //     countryDiv.innerHTML = `
+    //         <h1>${country.name}</h1>
+    //         <p>Population: ${country.population}</p>
+    //         <p>Area: ${country.area}</p>
+    //         <img src="${country.flag}">
+    //     `
+    // }
